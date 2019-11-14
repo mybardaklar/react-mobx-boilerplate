032ab4df22ecb0208cc8b66085ab21cf31ebf35f
Muhammed Yaşar Bardaklar <myasarbardaklar47@gmail.com>
2019-11-14 18:53:03 +0300

    build 0.0.0

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,45 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const bcrypt = require('bcrypt');
-
-var Users = require('../models/Users');
+var express = require('express');
+var router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
-
-  res.json(customers);
-});
-
-router.post('/add', (req, res, next) => {
-  const {fullname, username, password, email, admin} = req.body;
-
-  bcrypt.hash(password, 10, (err, hash) => {
-
-    const user = Users(
-      {
-        fullname,
-        username,
-        password: hash,
-        email,
-        admin
-      }
-    );
-
-    user.save(err => {
-      if(err) throw err;
-  
-      res.json({
-        status: true,
-        message: 'User saved successfully!'
-      });
-    });
-
-  });
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
